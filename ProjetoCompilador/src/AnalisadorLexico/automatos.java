dfef520a93430fd8d94530c7ed199f96face4253
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2019-08-17 20:54:34 -0300

    Automato (Quase pronto)
    foi feito poucos testes, mas ja consegue separar, colocar na pilha com o codigo de cada simbolo, para uma linha de if relativamente complexa

--- ProjetoCompilador/src/AnalisadorLexico/automatos.java
+++ ProjetoCompilador/src/AnalisadorLexico/automatos.java
@@ -6,22 +6,94 @@ import java.util.Stack;
 
 public class automatos {
 	
+	private Stack<pilha> simbols = new Stack<pilha>();
+	
+
 	private HashMap<String, String> tabelaSimbolos = new HashMap<String, String>(); 
-	
+
 	public boolean nativeSymbols(Character s) {
-			
-		return ";,+-/*:()[]0123456789".indexOf(s) != -1;
-		
+
+		return " ;,.+-/*:()[]".indexOf(s) != -1;
+
 	}
-	
+
 	public boolean nativeSequence(Character s) {
-		
+
 		return "=<>".indexOf(s) != -1;
-		
+
+	}
+
+
+	//if codigo23+1 then
+
+
+
+	public Stack<pilha> splitSimbols(String str, int linha){
+
+		String simbolo = "";
+		Character s;
+		String combinado = "";
+		//boolean isLastNative = false, isLastNativeSequence = false, isLastIdentifier = false;
+
+		//"if (20 >= 10)<>(10 = 10) then begin"
+		for(int i = 0; i < str.length(); i++) {
+			s = str.charAt(i); 
+			if (!nativeSymbols(s) /* || (Character.isDigit(s) && !isLastNative) ) && !Character.isWhitespace(s)*/) {
+				if(nativeSequence(s)) {
+					combinado = combinado + s;
+					if(nativeSequence(str.charAt(i+1))) {
+						combinado = combinado + str.charAt(i+1);
+						i++;
+					}
+					if(!simbolo.trim().isEmpty()) {
+						addInStack(simbolo, linha);
+						simbolo = "";
+					}
+					addInStack(combinado, linha);
+					combinado = "";					
+				}else {
+					simbolo = simbolo + s;
+					if(i+1 >= str.length()) {
+						addInStack(simbolo, linha);
+					}
+				}
+			}else {
+				if(!simbolo.trim().isEmpty()) {
+					addInStack(simbolo, linha);
+				}
+				if(!Character.isWhitespace(s)) {
+					addInStack(s.toString(), linha);					
+				}
+					
+					simbolo = "";
+					combinado = "";
+			}
+		}	
+
+
+
+		return simbols;
+
 	}
 	
-	public automatos() {
+	private void addInStack(String str, int linha) {
 		
+		simbols.add(new pilha(getSymbolID(str), linha, str));
+		
+	}
+
+	public Integer getSymbolID(String symbol) {
+
+		String str = tabelaSimbolos.get(symbol); 	
+		System.out.println("procurou "+symbol+" result " + str);
+
+		return ((str == null)) ? 25 : Integer.parseInt(str);
+
+
+	}
+
+	public automatos() {
+
 		tabelaSimbolos.put("program", "1");
 		tabelaSimbolos.put("label", "2");
 		tabelaSimbolos.put("const", "3");
@@ -31,6 +103,8 @@ public class automatos {
 		tabelaSimbolos.put("end", "7");
 		tabelaSimbolos.put("else", "15");
 		tabelaSimbolos.put("then", "14");
+		tabelaSimbolos.put("(", "36");
+		tabelaSimbolos.put(")", "37");
 		tabelaSimbolos.put(":=", "38");
 		tabelaSimbolos.put("=", "40");
 		tabelaSimbolos.put(">", "41");
@@ -43,51 +117,7 @@ public class automatos {
 		tabelaSimbolos.put("or", "22");
 		tabelaSimbolos.put("and", "23");
 		tabelaSimbolos.put("not", "24");
-		
-	}
-	
-	//if codigo23+1 then
-	
-	
-	
-	public Stack<pilha> splitSimbols(String str, int linha){
-		
-		pilha simbolo = new pilha(0, 0, "");
-		Stack<pilha> simbols = new Stack<pilha>();
-		Character s;
-		boolean isLastNative = false;
-		
-		for(int i = 0; i < str.length(); i++) {
-			s = str.charAt(i); 
-			if((!nativeSymbols(s) || (Character.isDigit(s) && !isLastNative)) && !Character.isWhitespace(s)) {
-				isLastNative = false;
-				simbolo.setSimbolo(simbolo.getSimbolo()+s);				
-			}else {
-				isLastNative = true;
-				if(!simbolo.getSimbolo().trim().isEmpty()) {
-					simbolo.setLinha(linha);
-					simbolo.setCodigo(getSymbolID(simbolo.getSimbolo()));
-					
-					simbols.add(simbolo);
-					simbolo = new pilha(0,  0,  "");
-				}									
-			}
-		}	
-		
-		
-		
-		return simbols;
-		
-	}
-	
-	public Integer getSymbolID(String symbol) {
-		
-		String str = tabelaSimbolos.get(symbol); 	
-		System.out.println("procurou "+symbol+" result " + str);
-		
-		return (str == "") ? 0 : Integer.parseInt(str);
-		
-		
+
 	}
 
 }
