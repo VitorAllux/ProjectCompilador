8942de0b3c20ce077705a5babb5aac6e0f92b8d0
Robson Moraes Cardoso <48536325+RobsonMora@users.noreply.github.com>
2019-11-04 21:55:50 -0300

    Criado nova classe de tokens

---
+++ src/Compilador/TokenSemantico.java
@@ -0,0 +1,25 @@
+package Compilador;
+
+public class TokenSemantico extends TokenX{
+	
+	private String tipo = "", categoria = "";
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
